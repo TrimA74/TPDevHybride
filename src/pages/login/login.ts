69966155cc9464fe616ec7ebc1e3b436650c3225
axelmov <brogna.axel@gmail.com>
2018-01-26 16:07:03 +0100

    connection

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+
+
+import { RestProvider } from '../../providers/rest/rest';
+import { Nav } from 'ionic-angular';
+
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  constructor() {
+
+  @ViewChild(Nav) nav: Nav;
+
+  private users:any;
+
+  constructor(public RestProvider : RestProvider) {
+
+    this.RestProvider.getData().then((data) => {
+     this. users = data;
+    });
+
+
+
+  }
+
+  doConnection(username, password) {
+    console.log('1');
+    this.users.forEach((user) => {
+      console.log(user);
+      if(user.name == username && user.pasword == password)
+      {
+        console.log('wtf');
+        this.nav.setRoot(ListPage);
+      }
+
+
+    });
 
   }
 }
